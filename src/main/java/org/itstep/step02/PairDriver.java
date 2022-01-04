4f79e2c71f65d5bbf9f3e1a9cf0a694038fa9f83
sergeybiophys <roven2hati@gmail.com>
2022-01-04 14:11:10 +0200

    task2 completed

--- src/main/java/org/itstep/step02/PairDriver.java
+++ src/main/java/org/itstep/step02/PairDriver.java
@@ -1,6 +1,28 @@
 package org.itstep.step02;
 
+import org.itstep.step01.ObjectPair;
+
 // TODO: Пишите ваш код здесь
 public class PairDriver {
+    Pair[] stadiums = new Pair[3];
 
+
+    public static String largestStadium(Pair[] stadiums) {
+        // TODO: реализуйте это метод в соответствии с комментариями
+        int max = (int) stadiums[0].getSecond();
+        String maxName = (String)stadiums[0].getFirst();
+        for(int i = 0; i<stadiums.length; i++)
+        {
+            String tmpData = stadiums[i].getSecond().toString().replaceAll("[,]","");
+            //TODO remove the comma
+            Integer tmpInt = Integer.parseInt(tmpData);
+            //float tmp = (float)item.getSecond();
+            if(tmpInt>max)
+            {
+                max = tmpInt;
+                maxName = (String)stadiums[i].getFirst();
+            }
+        }
+        return maxName;
+    }
 }
--- src/test/java/org/itstep/step02/PairDriverTest.java
+++ src/test/java/org/itstep/step02/PairDriverTest.java
@@ -10,31 +10,31 @@ class PairDriverTest {
 
     @Test
     public void testPairDriverLargestStadiumLargestAtEnd() {
-        fail("Раскомментируйте тест после реализации класса PairDriver");
-//        Pair<String, Integer>[] stadiums = new Pair[3];
-//        stadiums[0] = new Pair<String, Integer>("A", 1);
-//        stadiums[1] = new Pair<String, Integer>("B", 2);
-//        stadiums[2] = new Pair<String, Integer>("C", 3);
-//        assertEquals("C", PairDriver.largestStadium(stadiums));
+        //fail("Раскомментируйте тест после реализации класса PairDriver");
+        Pair<String, Integer>[] stadiums = new Pair[3];
+        stadiums[0] = new Pair<String, Integer>("A", 1);
+        stadiums[1] = new Pair<String, Integer>("B", 2);
+        stadiums[2] = new Pair<String, Integer>("C", 3);
+        assertEquals("C", PairDriver.largestStadium(stadiums));
     }
 
     @Test
     public void testPairDriverLargestStadiumLargestAtStart() {
-        fail("Раскомментируйте тест после реализации класса PairDriver");
-//        Pair<String, Integer>[] stadiums = new Pair[3];
-//        stadiums[0] = new Pair<String, Integer>("A", 3);
-//        stadiums[1] = new Pair<String, Integer>("B", 2);
-//        stadiums[2] = new Pair<String, Integer>("C", 1);
-//        assertEquals("A", PairDriver.largestStadium(stadiums));
+        //fail("Раскомментируйте тест после реализации класса PairDriver");
+        Pair<String, Integer>[] stadiums = new Pair[3];
+        stadiums[0] = new Pair<String, Integer>("A", 3);
+        stadiums[1] = new Pair<String, Integer>("B", 2);
+        stadiums[2] = new Pair<String, Integer>("C", 1);
+        assertEquals("A", PairDriver.largestStadium(stadiums));
     }
 
     @Test
     public void testPairDriverLargestStadiumLargestInMiddle() {
-        fail("Раскомментируйте тест после реализации класса PairDriver");
-//        Pair<String, Integer>[] stadiums = new Pair[3];
-//        stadiums[0] = new Pair<String, Integer>("A", 3);
-//        stadiums[1] = new Pair<String, Integer>("B", 1);
-//        stadiums[2] = new Pair<String, Integer>("C", 2);
-//        assertEquals("A", PairDriver.largestStadium(stadiums));
+        //fail("Раскомментируйте тест после реализации класса PairDriver");
+        Pair<String, Integer>[] stadiums = new Pair[3];
+        stadiums[0] = new Pair<String, Integer>("A", 3);
+        stadiums[1] = new Pair<String, Integer>("B", 1);
+        stadiums[2] = new Pair<String, Integer>("C", 2);
+        assertEquals("A", PairDriver.largestStadium(stadiums));
     }
 }
