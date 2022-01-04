1f89710191d268f8781f4a8027c1eb32b0adf86c
sergeybiophys <rove2hati@gmail.com>
2022-01-04 22:06:40 +0200

    invalidate caches

--- src/main/java/org/itstep/step03/PairsDriver.java
+++ src/main/java/org/itstep/step03/PairsDriver.java
@@ -1,5 +1,7 @@
 package org.itstep.step03;
 
+import org.itstep.step02.Pair;
+
 import java.util.Iterator;
 
 /**
@@ -13,39 +15,39 @@ public class PairsDriver {
 
     public static void main(String[] args) {
         // TODO: раскомментируйте код и запустите
-//        Pairs<String, Integer> pairs = new Pairs<String, Integer>();
-//        for (int i = 0; i < 15; i++) {
-//            pairs.addPair("A" + i, i);
-//        }
-//
-//        /* Перебрать все и распечатать */
-//        for (Pair<String, Integer> pair : pairs) {
-//            System.out.println(pair.toString());
-//        }
-//        System.out.println();
-//
-//        Iterator<Pair<String, Integer>> iterator = pairs.iterator();
-//        while (iterator.hasNext()) {
-//            Pair<String, Integer> pair = iterator.next();
-//            /* Удалите любые с четным вторым значением */
-//            if ((pair.getSecond() % 2) == 0) {
-//                iterator.remove();
-//            }
-//        }
-//
-//        /* Повторите еще раз и распечатайте оставшиеся */
-//        for (Pair<String, Integer> pair : pairs) {
-//            System.out.println(pair.toString());
-//        }
-//        System.out.println();
-//
-//        /* Теперь добавьте что-то еще и проверьте, что это было добавлено. */
-//        pairs.addPair("B", 10);
-//
-//        for (Pair<String, Integer> pair : pairs) {
-//            System.out.println(pair.toString());
-//        }
-//        System.out.println();
+        Pairs<String, Integer> pairs = new Pairs<String, Integer>();
+        for (int i = 0; i < 15; i++) {
+            pairs.addPair("A" + i, i);
+        }
+
+        /* Перебрать все и распечатать */
+        for (Pair<String, Integer> pair : pairs) {
+            System.out.println(pair.toString());
+        }
+        System.out.println();
+
+        Iterator<Pair<String, Integer>> iterator = pairs.iterator();
+        while (iterator.hasNext()) {
+            Pair<String, Integer> pair = iterator.next();
+            /* Удалите любые с четным вторым значением */
+            if ((pair.getSecond() % 2) == 0) {
+                iterator.remove();
+            }
+        }
+
+        /* Повторите еще раз и распечатайте оставшиеся */
+        for (Pair<String, Integer> pair : pairs) {
+            System.out.println(pair.toString());
+        }
+        System.out.println();
+
+        /* Теперь добавьте что-то еще и проверьте, что это было добавлено. */
+        pairs.addPair("B", 10);
+
+        for (Pair<String, Integer> pair : pairs) {
+            System.out.println(pair.toString());
+        }
+        System.out.println();
 
     }
 }
